1521d90039934045b9f9ec60d43ba4b470479add
diablo2532 <nikitos2532@gmail.com>
2026-09-07 21:26:34 +0200

    Add first task

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+//Створення User   Task первий 
+const user =  {
+    firstName: "Sem", 
+    lastName: "Semovich",
+    emailUser: "sem_semovich@mail.com",
+    passwordUser: "qwerty",
+    phoneNrUser: "+48 324 124 214",
+    adressUser:  {
+        cityAdress: "Paris",
+        streetAdress: "Adamowicha 25",
+        houseAdress: 243,
+        apartamentAdress: 12,
+    },
+};
+console.log('user', user);
