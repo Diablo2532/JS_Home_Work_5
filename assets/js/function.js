19475820c498869e3a2af00410b77b7b0b429f82
diablo2532 <nikitos2532@gmail.com>
2026-09-07 21:47:55 +0200

    Create function for greeting

---
+++ assets/js/function.js
@@ -0,0 +1,10 @@
+/**
+ * Виводить привітання користувачу з його ім'ям та прізвищем.
+ * 
+ * @param {string} firsName Ім'я користувача.
+ * @param {string} lastName Прізвище користувача.
+ */
+function greetingUser (firsName, lastName ) {
+    alert(`Доброго дня, ${firsName} ${lastName}`);
+    
+};
