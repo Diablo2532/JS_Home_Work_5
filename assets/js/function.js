1575464861c7e50892640c48dbe299119ece938e
diablo2532 <nikitos2532@gmail.com>
2026-09-13 15:16:14 +0200

    Edit name function

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,11 +1,11 @@
 /**
  * Виводить привітання користувачу з його ім'ям та прізвищем.
  * 
- * @param {string} firsName Ім'я користувача.
+ * @param {string} firstName Ім'я користувача.
  * @param {string} lastName Прізвище користувача.
  * @returns {string} Рядок із привітанням.
  */
-function greetingUser (firsName, lastName ) {
-    return `Доброго дня, ${firsName} ${lastName}`;
+function greetingUser (firstName, lastName ) {
+    return `Доброго дня, ${firstName} ${lastName}`;
     
 };
